81825de771f33934f108175f632b8133f2c6db29
jeanmadao <huynhjeanpierre@hotmail.com>
2024-01-03 20:56:27 +0100

    player completed

--- .config/ags/js/player.js
+++ .config/ags/js/player.js
@@ -3,27 +3,73 @@ import Mpris from 'resource:///com/github/Aylur/ags/service/mpris.js';
 
 const Player = (vertical) => Widget.Box({
     className: "player",
-    vertical: true,
     children: [
-        Widget.Label({
-            hpack: "start",
+        Widget.Box({
+            className: "album-cover",
+            css: ".album-cover { background-image: url('/home/feilong/.config/ags/assets/default-cover.png') }",
+            children: [
+                Widget.Button({
+                    className: "player-btn",
+                    child: Widget.Icon({
+                        className: "player-icon",
+                        size: 20,
+                    })
+                })
+            ]
         }),
-        Widget.Label({
-            hpack: "start",
+        !vertical && Widget.Box({
+            className: "track-info",
+            vertical: true,
+            vpack: "center",
+            children: [
+                Widget.Label({
+                    hpack: "start",
+                    label: "Have a nice day"
+
+                }),
+                Widget.Label({
+                    hpack: "start",
+                    label: "Feilong !"
+                })
+            ]
         })
     ],
     setup: self => self.hook(Mpris, self => {
         const player = Mpris.getPlayer("spotify")
+
+        const boxCover = self.children[0]
+        const playButton = boxCover.children[0]
+        const playIcon = playButton.child
+        playButton.onClicked = () => player.playPause()
+
+
         if (player) {
+            if (player.playBackStatus === "Playing") {
+                playIcon.icon= "media-playback-pause-symbolic"
+                self.className = "player playing"
+            } else if (player.playBackStatus === "Paused") {
+                playIcon.icon= "media-playback-start-symbolic"
+                self.className = "player paused"
+            }
+            if (player.coverPath) {
+                boxCover.css = `.album-cover { background-image: url("${player.coverPath}") }`
+            }
             if (!vertical) {
-                self.children[0].label = `${player.trackTitle}`
+                const [trackLabel, artistLabel] = self.children[1].children
+                trackLabel.label = `${player.trackTitle}`
                 if (player.trackArtists) {
-                    self.children[1].label = `${player.trackArtists.join(", ")}`
+                    artistLabel.label = `${player.trackArtists.join(", ")}`
                 }
             }
         } else {
-            self.children[0].label = ""
-            self.children[1].label = ""
+            if (!vertical) {
+                const [trackLabel, artistLabel] = self.children[1].children
+                trackLabel.label = "Have a nice day"
+                artistLabel.label = "Feilong!"
+            }
+            self.className = "player"
+            boxCover.css = ".album-cover { background-image: url('/home/feilong/.config/ags/assets/default-cover.png') }"
+
         }
     })
 })
