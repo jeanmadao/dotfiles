3bbc21fbbaab8c325745ae47a5ceb9188e12a417
jeanmadao <huynhjeanpierre@hotmail.com>
2024-01-05 22:52:09 +0100

    use native menu to fix commands autocomplete

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -41,25 +41,6 @@ return {
                     { name = 'buffer' },
                 })
             })
-
-            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = 'buffer' }
-                }
-            })
-
-            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                },
-                {
-                    { name = 'cmdline' }
-                })
-            })
         end,
     }
 }
