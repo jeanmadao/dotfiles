04bb7a56163d911096550090ee79f4ac4858aed1
jeanmadao <huynhjeanpierre@hotmail.com>
2024-08-12 09:15:49 +0200

    removed launcher widget

--- .config/ags/main.ts
+++ .config/ags/main.ts
@@ -1,11 +1,9 @@
 import "style/style"
 import { forMonitors } from "lib/utils"
 import Bar from "widget/bar/Bar"
-import Launcher from "widget/launcher/Launcher"
 
 App.config({
     windows: [
         ...forMonitors(Bar),
-        //Launcher()
     ],
 })
