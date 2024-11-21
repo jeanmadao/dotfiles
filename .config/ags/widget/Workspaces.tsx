e55f35933609dc9921e7d11955c7eaa0f9807619
jeanmadao <huynhjeanpierre@hotmail.com>
2024-11-22 00:45:45 +0100

    weeaboo workspace name

--- .config/ags/widget/Workspaces.tsx
+++ .config/ags/widget/Workspaces.tsx
@@ -4,6 +4,8 @@ const hyprland = AstalHyprland.get_default()
 
 const dispatch = (ws: number) => hyprland.message_async(`dispatch workspace ${ws}`, null);
 
+const workspaces_names = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十"]
+
 
 const Workspaces = () => {
     return <box
@@ -11,7 +13,7 @@ const Workspaces = () => {
         spacing={5}>
         {Array.from({ length: 10 }, (_, i) => i + 1).map(i => <button
             className="Workspace"
-            label={String(i)} 
+            label={workspaces_names[i-1]} 
             setup={self => self.hook(hyprland, "event", () => {
                 self.toggleClassName("active", hyprland.get_focused_workspace().get_id() === i)
                 self.toggleClassName("occupied", (hyprland.get_workspace(i)?.get_clients().length || 0) > 0)
