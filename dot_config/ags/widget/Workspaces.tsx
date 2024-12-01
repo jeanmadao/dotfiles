a3f51f7b1c521869737fe6a2ac698b972755c290
Jean Huynh <jeanpierre@proton.me>
2024-12-01 17:47:13 +0100

    remove buggy get_workspace() from code

--- dot_config/ags/widget/Workspaces.tsx
+++ dot_config/ags/widget/Workspaces.tsx
@@ -10,14 +10,19 @@ const workspaces_names = ["一", "二", "三", "四", "五", "六", "七", "八"
 const Workspaces = () => {
     return <box
         className="Workspaces Widget"
-        spacing={5}>
+        spacing={5}
+        setup={self=> self.hook(hyprland, "event", () => {
+            const focused_id = hyprland.get_focused_workspace().get_id()
+            const workspaces_id = hyprland.get_workspaces().map(ws => ws.get_id())
+            self.children.forEach((btn, i) => {
+                btn.toggleClassName("active", i + 1 === focused_id)
+                btn.toggleClassName("occupied", workspaces_id.includes(i + 1))
+            })
+        })}>
+
         {Array.from({ length: 10 }, (_, i) => i + 1).map(i => <button
             className="Workspace"
             label={workspaces_names[i-1]} 
-            setup={self => self.hook(hyprland, "event", (_, event, args) => {
-                self.toggleClassName("active", hyprland.get_focused_workspace().get_id() === i)
-                self.toggleClassName("occupied", (hyprland.get_workspace(i)?.get_clients().length || 0) > 0)
-            })}
             onClick={() => dispatch(i)} />
         )}
     </box>
